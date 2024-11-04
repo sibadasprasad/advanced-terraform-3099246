2c40739b7248d07ddfed74cab861929e8767a47d
sibeindra <sibeindra@gmail.com>
2024-11-04 17:03:44 +0530

    modified project name

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "advanceterraform-440708" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
 
 ### NETWORK
 data "google_compute_network" "default" {
-  name                    = "default"
+  name = "default"
 }
 
 ## SUBNET
@@ -40,8 +40,8 @@ resource "google_compute_firewall" "default" {
 resource "google_compute_instance" "nginx_instance" {
   name         = "nginx-proxy"
   machine_type = "f1-micro"
-  tags = ["web"]
-  
+  tags         = ["web"]
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -49,10 +49,10 @@ resource "google_compute_instance" "nginx_instance" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network    = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
     access_config {
-      
+
     }
   }
 }
@@ -61,7 +61,7 @@ resource "google_compute_instance" "nginx_instance" {
 resource "google_compute_instance" "web1" {
   name         = "web1"
   machine_type = "f1-micro"
-  
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -70,7 +70,7 @@ resource "google_compute_instance" "web1" {
 
   network_interface {
     # A default network is created for all GCP projects
-    network = data.google_compute_network.default.self_link
+    network    = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }
 }
@@ -78,7 +78,7 @@ resource "google_compute_instance" "web1" {
 resource "google_compute_instance" "web2" {
   name         = "web2"
   machine_type = "f1-micro"
-  
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -86,7 +86,7 @@ resource "google_compute_instance" "web2" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network    = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }
 }
@@ -94,7 +94,7 @@ resource "google_compute_instance" "web2" {
 resource "google_compute_instance" "web3" {
   name         = "web3"
   machine_type = "f1-micro"
-  
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -102,16 +102,16 @@ resource "google_compute_instance" "web3" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network    = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
+  }
 }
 
 ## DB
 resource "google_compute_instance" "mysqldb" {
   name         = "mysqldb"
   machine_type = "f1-micro"
-  
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -119,7 +119,7 @@ resource "google_compute_instance" "mysqldb" {
   }
 
   network_interface {
-    network = data.google_compute_network.default.self_link
+    network    = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
+  }
 }
